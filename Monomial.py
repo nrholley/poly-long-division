a8e9377c0245863c09fdace970bc44f39e753d44
nolanrholley <nrh2@williams.edu>
2023-05-12 14:25:00 -0400

    add function to compute difference of monomial powers

--- Monomial.py
+++ Monomial.py
@@ -26,7 +26,7 @@ class Monomial:
         return self.coefficient
 
 
-    def vars(self):
+    def get_vars(self):
         return self.vars.copy()
 
 
@@ -41,14 +41,38 @@ class Monomial:
         return self.str_representation
 
 
+    def __eq__(self, other):
+        return self.vars == other.vars and self.coefficient == other.coefficient
+
+
+def difference_vector(variables, a, b):
+    """
+    :param variables: the variables, in order, string format. E.g. "xyz"
+    :param a: first monomial
+    :param b: second monomial
+    :return: tuple of the differences of variable powers, corresponding to alpha - beta in the textbook/handout
+    """
+    diff = []
+    for v in variables:
+        a_vars = a.get_vars()
+        b_vars = b.get_vars()
+        a_pow = a_vars[v] if v in a_vars else 0
+        b_pow = b_vars[v] if v in b_vars else 0
+        diff.append(a_pow - b_pow)
+    return tuple(diff)
+
+
 # for testing
 if __name__ == '__main__':
-    string = "5*x^3*y*z*x_0^7"
+    string = "5*x^5*y^4*z"
+    string2 = "y*z^2"
     m = Monomial(string)
-    print(m)
-    print(m.vars)
-    print(m.total_degree())
-    print(m.get_coefficient())
+    m2 = Monomial(string2)
+    print(difference_vector("xyz", m, m2))
+    # print(m)
+    # print(m.vars)
+    # print(m.total_degree())
+    # print(m.get_coefficient())
 
 
 
